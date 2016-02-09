3487be7cba7e35e582542086e0ede7db73963f46
Richard <richard.anaya@gmail.com>
2016-02-08 22:15:40 -0600

    adding test and setup

---
+++ patternmatch.js
@@ -0,0 +1,22 @@
+// patternmatch.js
+// repo    : https://github.com/richardanaya/patternmatch
+// license : MIT
+
+(function (window, module) {
+  "use strict";
+  function pattern(){
+
+  }
+
+  function match(){
+
+  }
+
+  window.matches = module.exports = {
+    pattern  : pattern,
+    match    : match
+  };
+})(
+  typeof window !== "undefined" ? window : {},
+  typeof module !== "undefined" ? module : {}
+);
